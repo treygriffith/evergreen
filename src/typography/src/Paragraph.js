7a9270b4e898a8519fe8048ec8054dbcb7a6d950
Allen Kleiner <akleiner24@gmail.com>
2020-05-13 10:44:31 -0700

    Convert typography components (Paragraph, Strong, Text) to memo + forwardRef

--- src/typography/src/Paragraph.js
+++ src/typography/src/Paragraph.js
@@ -1,41 +1,19 @@
-import React, { PureComponent } from 'react'
+import React, { memo, forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import Box from 'ui-box'
-import { withTheme } from '../../theme'
+import { useTheme } from '../../theme'
 
-class Paragraph extends PureComponent {
-  static propTypes = {
-    /**
-     * Composes the Box component as the base.
-     */
-    ...Box.propTypes,
+const Paragraph = memo(
+  forwardRef((props, ref) => {
+    const {
+      size = 400,
+      color = 'default',
+      fontFamily = 'ui',
+      marginTop,
+      ...rest
+    } = props
 
-    /**
-     * Size of the text style.
-     * Can be: 300, 400, 500.
-     */
-    size: PropTypes.oneOf([300, 400, 500]).isRequired,
-
-    /**
-     * Font family.
-     * Can be: `ui`, `display` or `mono` or a custom font family.
-     */
-    fontFamily: PropTypes.string.isRequired,
-
-    /**
-     * Theme provided by ThemeProvider.
-     */
-    theme: PropTypes.object.isRequired
-  }
-
-  static defaultProps = {
-    size: 400,
-    color: 'default',
-    fontFamily: 'ui'
-  }
-
-  render() {
-    const { theme, size, color, fontFamily, marginTop, ...props } = this.props
+    const theme = useTheme()
 
     const {
       marginTop: defaultMarginTop,
@@ -48,15 +26,40 @@ class Paragraph extends PureComponent {
     return (
       <Box
         is="p"
+        ref={ref}
         color={theme.getTextColor(color)}
         fontFamily={theme.getFontFamily(fontFamily)}
         marginTop={finalMarginTop || 0}
         marginBottom={0}
         {...textStyle}
-        {...props}
+        {...rest}
       />
     )
-  }
+  })
+)
+
+Paragraph.propTypes = {
+  /**
+   * Composes the Box component as the base.
+   */
+  ...Box.propTypes,
+
+  /**
+   * Size of the text style.
+   * Can be: 300, 400, 500.
+   */
+  size: PropTypes.oneOf([300, 400, 500]).isRequired,
+
+  /**
+   * Font family.
+   * Can be: `ui`, `display` or `mono` or a custom font family.
+   */
+  fontFamily: PropTypes.string.isRequired,
+
+  /**
+   * Theme provided by ThemeProvider.
+   */
+  theme: PropTypes.object.isRequired
 }
 
-export default withTheme(Paragraph)
+export default Paragraph
--- src/typography/src/Text.js
+++ src/typography/src/Text.js
@@ -1,52 +1,23 @@
+import React, { memo, forwardRef } from 'react'
 import cx from 'classnames'
 import { css as glamorCss } from 'glamor'
-import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Box from 'ui-box'
-import { withTheme } from '../../theme'
+import { useTheme } from '../../theme'
 
-class Text extends PureComponent {
-  static propTypes = {
-    /**
-     * Composes the Box component as the base.
-     */
-    ...Box.propTypes,
-
-    /**
-     * Size of the text style.
-     * Can be: 300, 400, 500, 600.
-     */
-    size: PropTypes.oneOf([300, 400, 500, 600]).isRequired,
-
-    /**
-     * Font family.
-     * Can be: `ui`, `display` or `mono` or a custom font family.
-     */
-    fontFamily: PropTypes.string.isRequired,
-
-    /**
-     * Theme provided by ThemeProvider.
-     */
-    theme: PropTypes.object.isRequired
-  }
-
-  static defaultProps = {
-    size: 400,
-    color: 'default',
-    fontFamily: 'ui'
-  }
-
-  render() {
+const Text = memo(
+  forwardRef((props, ref) => {
     const {
+      size = 400,
+      color = 'default',
+      fontFamily = 'ui',
+      marginTop,
       className,
       css,
-      theme,
-      size,
-      color,
-      fontFamily,
-      marginTop,
-      ...props
-    } = this.props
+      ...rest
+    } = props
+
+    const theme = useTheme()
 
     const { marginTop: defaultMarginTop, ...textStyle } = theme.getTextStyle(
       size
@@ -58,15 +29,40 @@ class Text extends PureComponent {
     return (
       <Box
         is="span"
+        ref={ref}
         color={theme.getTextColor(color)}
         fontFamily={theme.getFontFamily(fontFamily)}
         marginTop={finalMarginTop}
         {...textStyle}
         className={cx(className, css ? glamorCss(css).toString() : undefined)}
-        {...props}
+        {...rest}
       />
     )
-  }
+  })
+)
+
+Text.propTypes = {
+  /**
+   * Composes the Box component as the base.
+   */
+  ...Box.propTypes,
+
+  /**
+   * Size of the text style.
+   * Can be: 300, 400, 500.
+   */
+  size: PropTypes.oneOf([300, 400, 500]).isRequired,
+
+  /**
+   * Font family.
+   * Can be: `ui`, `display` or `mono` or a custom font family.
+   */
+  fontFamily: PropTypes.string.isRequired,
+
+  /**
+   * Theme provided by ThemeProvider.
+   */
+  theme: PropTypes.object.isRequired
 }
 
-export default withTheme(Text)
+export default Text
