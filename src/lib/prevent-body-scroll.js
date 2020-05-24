3305c639e1f7b212ccfebc593fa8391a2f99dae6
Matt Shwery <mshwery@gmail.com>
2020-05-23 23:29:21 -0500

    convert prevent body scroll into hook

--- src/lib/prevent-body-scroll.js
+++ src/lib/prevent-body-scroll.js
@@ -1,33 +1,41 @@
-let previousOverflow
-let previousPaddingRight
+import { useCallback, useEffect, useState } from 'react'
 
 /**
  * Toggle the body scroll / overflow and additional styling
  * necessary to preserve scroll position and body width (scrollbar replacement)
- *
- * @param {boolean} preventScroll - whether or not to prevent body scrolling
+ * @param {boolean} isScrollInitiallyLocked - whether or not to prevent body scrolling
  */
-export default function preventBodyScroll(preventScroll) {
-  /** Get the width before toggling the style so we can calculate the scrollbar width for a smooth, jankless style change */
-  const { width } = document.body.getBoundingClientRect()
+export default function usePreventBodyScroll(isScrollInitiallyLocked = false) {
+  const { overflow, paddingRight } = window.getComputedStyle(document.body)
+  const [isScrollLocked, setScrollLock] = useState(isScrollInitiallyLocked)
+  const [originalOverflow] = useState(overflow || '')
+  const [originalPaddingRight] = useState(paddingRight || '')
 
-  /** Apply or remove overflow style */
-  if (preventScroll) {
-    previousOverflow = document.body.style.overflow
-    document.body.style.overflow = 'hidden'
-  } else {
-    document.body.style.overflow = previousOverflow || ''
-  }
+  const resetStyles = useCallback(() => {
+    document.body.style.overflow = originalOverflow
+    document.body.style.paddingRight = originalPaddingRight
+  }, [originalOverflow, originalPaddingRight])
 
-  /** Get the _new width_ of the body (this will tell us the scrollbar width) */
-  const newWidth = document.body.getBoundingClientRect().width
-  const scrollBarWidth = newWidth - width
+  useEffect(() => {
+    /** Get the width before toggling the style so we can calculate the scrollbar width for a smooth, jankless style change */
+    const originalWidth = document.body.getBoundingClientRect().width
 
-  /** If there's a diff due to scrollbars, then account for it with padding */
-  if (preventScroll) {
-    previousPaddingRight = document.body.style.paddingRight
-    document.body.style.paddingRight = Math.max(0, scrollBarWidth || 0) + 'px'
-  } else {
-    document.body.style.paddingRight = previousPaddingRight || ''
-  }
+    /** Apply or remove overflow style */
+    if (isScrollLocked) {
+      document.body.style.overflow = 'hidden'
+
+      /** Get the _new width_ of the body (this will tell us the scrollbar width) */
+      const newWidth = document.body.getBoundingClientRect().width
+      const scrollBarWidth = newWidth - originalWidth
+
+      /** If there's a diff due to scrollbars, then account for it with padding */
+      document.body.style.paddingRight = Math.max(0, scrollBarWidth || 0) + 'px'
+    } else {
+      resetStyles()
+    }
+
+    return resetStyles
+  }, [isScrollLocked])
+
+  return [isScrollLocked, setScrollLock]
 }
